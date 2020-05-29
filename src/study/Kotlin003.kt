13e5ac046b25c431b921a1b2228fcb547abd7f0a
lingxiaodshz <lingxiaodshz@163.com>
2020-05-29 10:06:18 +0800

    双线程打印奇偶数

---
+++ src/study/Kotlin003.kt
@@ -0,0 +1,81 @@
+package stud
+
+import java.lang.Thread.sleep
+
+
+/**
+ * kotlin线程
+ */
+
+fun main(args: Array<String>) {
+
+    //集成Thread类
+//    object : Thread() {
+//        override fun run() {
+//            var i = 1
+//            while (i < 10) {
+//                println(i)
+//                sleep(300L)
+//                i++
+//            }
+//        }
+//    }.start()
+
+//    Thread(object : Runnable {
+//        override fun run() {
+//            var i = 1
+//            while (i < 10) {
+//                println(i)
+//                sleep(300L)
+//                i++
+//            }
+//        }
+//    }).start()
+
+    var num = Num()
+    var obj = java.lang.Object()
+
+    var odd = object : Runnable {
+        override fun run() {
+            while (num.i < 10) {
+                synchronized(obj) {
+                    if (num.flag) {
+                        println(Thread.currentThread().name + "  " + num.i)
+                        num.i++
+                        num.flag = false
+                        sleep(300L)
+                        obj.notify()
+                    } else {
+                        obj.wait()
+                    }
+                }
+            }
+        }
+    }
+    var eve = object : Runnable {
+        override fun run() {
+            while (num.i < 10) {
+                synchronized(obj) {
+                    if (!num.flag) {
+                        println(Thread.currentThread().name + "  " + num.i)
+                        num.i++
+                        num.flag = true
+                        sleep(300L)
+                        obj.notify()
+                    } else {
+                        obj.wait()
+                    }
+                }
+            }
+        }
+    }
+
+    Thread(odd).start()
+    Thread(eve).start()
+}
+
+class Num {
+    var i = 1
+    var flag = false
+}
+
